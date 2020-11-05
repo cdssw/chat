10b35ed2b9b199b0fa055ca996ca9bea4aae00fd
cdssw <cdssw@naver.com>
2020-11-05 15:58:38 +0900

    test

--- src/main/java/com/moim/chat/controller/ChatController.java
+++ src/main/java/com/moim/chat/controller/ChatController.java
@@ -20,6 +20,7 @@ import com.moim.chat.service.chat.ChatDto;
 import com.moim.chat.service.chat.ChatService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * ChatController.java
@@ -36,6 +37,7 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 @RestController
+@Slf4j
 public class ChatController {
 	
 	private ChatService chatService;
@@ -67,6 +69,7 @@ public class ChatController {
 	@ResponseStatus(value = HttpStatus.OK)
 	public List<ChatDto.UsersUnreadRes> getUsersUnread(@PathVariable final long meetId, HttpServletRequest req) {
 		String username = req.getHeader("username"); // gateway에서 보내준 username header를 추출
+		log.debug(username);
 		return chatService.getUsersUnread(meetId, username);
 	}
 }
