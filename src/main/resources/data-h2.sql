43bcce660740def1194b313aea532e3308e7866b
cdssw <cdssw@naver.com>
2020-11-05 16:47:58 +0900

    중복 계정 추가 제거

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,9 +1,3 @@
--- user
-insert into user(id, username, user_nm, user_nick_nm, phone, time_stamp) values
-(1, 'cdssw@naver.com', 'Andrew', 'Blue', '010-1111-1111', now())
-, (2, 'loh002@naver.com', 'Monica', 'Red','010-1111-1111', now())
-, (3, 'michael@naver.com', 'Michael', 'Green','010-1111-1111', now());
-
 -- chat
 insert into chat(leader_name, sender, receiver, meet_id, message, read_yn, time_stamp) values
   ('cdssw@naver.com', 'loh002@naver.com', 'cdssw@naver.com',  1, 'msg 1', false, now())
