222fa9ecc9fd36d4cb4282d4bd98d5261d26a3bd
cdssw <cdssw@naver.com>
2020-11-26 16:55:30 +0900

    테스트 데이터 수정

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,6 @@
 -- user
 insert into user(id, username, user_nm, user_nick_nm, phone, time_stamp) values
 (1, 'cdssw@naver.com', 'Andrew', 'Blue', '010-1111-1111', now())
-, (2, 'loh002@naver.com', 'Monica', 'Red','010-1111-1111', now())
-, (3, 'michael@naver.com', 'Michael', 'Green','010-1111-1111', now());
+, (2, 'loh002@naver.com', 'Monica', 'HK','010-1111-1111', now())
+, (3, 'kimkh093@nate.com', '김규현', 'Developer','010-1111-1111', now())
+, (4, 'michael@naver.com', 'Michael', 'Green','010-1111-1111', now());
